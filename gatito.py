096f78e13ef70f3232322162575e464cd695efd6
Jaredto22 <brandiskiyaret3@gmail.com>
2026-05-09 01:15:41 -0600

    config render

--- gatito.py
+++ gatito.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('gatito.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
