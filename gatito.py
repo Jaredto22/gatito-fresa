ab0e44850387aa4d5d8f748d7787b4f3b262cdcd
Jaredto22 <brandiskiyaret3@gmail.com>
2026-05-09 01:32:29 -0600

    config render

--- gatito.py
+++ gatito.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('gatito.html')
 
 if __name__ == '__main__':
     app.run(
